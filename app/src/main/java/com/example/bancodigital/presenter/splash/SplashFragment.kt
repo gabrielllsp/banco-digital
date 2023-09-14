f13f3ffccac15c9b941cb00ce4a6fe65cc99d287
gabri <gabriel.amoreira@outlook.com>
2023-09-14 11:59:09 -0300

    Criando layout register.

---
+++ app/src/main/java/com/example/bancodigital/presenter/splash/SplashFragment.kt
@@ -0,0 +1,30 @@
+package com.example.bancodigital.presenter.splash
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.bancodigital.R
+import com.example.bancodigital.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
+}
